aba690f182fdda5734b13304870583774e40f6ee
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-25 22:56:53 +0300

    fix(user): correct typo in error message

--- src/main/java/com/ist/internal_issue_tracker/user/exception/UserErrorCode.java
+++ src/main/java/com/ist/internal_issue_tracker/user/exception/UserErrorCode.java
@@ -4,7 +4,7 @@ import com.ist.internal_issue_tracker.shared.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum UserErrorCode implements ErrorCode {
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "This email already exist.");
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "This email already exists.");
 
     private final HttpStatus httpStatus;
     private final String message;
