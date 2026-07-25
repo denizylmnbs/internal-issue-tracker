e5f77a2e3783d6242da38c0819a41ac089750909
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-25 23:42:44 +0300

    feat(user): implement create user REST endpoint

--- src/main/java/com/ist/internal_issue_tracker/user/UserController.java
+++ src/main/java/com/ist/internal_issue_tracker/user/UserController.java
@@ -1,4 +1,29 @@
 package com.ist.internal_issue_tracker.user;
 
+import com.ist.internal_issue_tracker.shared.web.ApiResponse;
+import com.ist.internal_issue_tracker.user.dto.UserCreateRequest;
+import com.ist.internal_issue_tracker.user.dto.UserResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<UserResponse>> createUser(@Valid @RequestBody UserCreateRequest request) {
+
+        UserResponse userResponse = userService.createUser(request);
+
+        return ResponseEntity.created(URI.create("/api/users/" + userResponse.id())).body(ApiResponse.ok(userResponse));
+    }
 }
