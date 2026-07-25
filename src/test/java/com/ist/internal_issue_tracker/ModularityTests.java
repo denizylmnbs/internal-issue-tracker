9268f3b2132e3b319bfd3dad0f8695b65355a295
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-25 17:47:46 +0300

    test: add spring modulith structure verification test

---
+++ src/test/java/com/ist/internal_issue_tracker/ModularityTests.java
@@ -0,0 +1,23 @@
+package com.ist.internal_issue_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ModularityTests {
+
+    ApplicationModules modules = ApplicationModules.of(InternalIssueTrackerApplication.class);
+
+    @Test
+    void verifiesModularStructure() {
+        modules.verify();
+    }
+
+    @Test
+    void writeDocumentationSnippets() {
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeIndividualModulesAsPlantUml();
+    }
+
+}
