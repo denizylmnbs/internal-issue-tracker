1c31917c524aaf99d14bd5a591b5ad1c999534ad
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-25 16:40:35 +0300

    Initial commit

---
+++ src/main/java/com/ist/internal_issue_tracker/InternalIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ist.internal_issue_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalIssueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalIssueTrackerApplication.class, args);
+    }
+
+}
