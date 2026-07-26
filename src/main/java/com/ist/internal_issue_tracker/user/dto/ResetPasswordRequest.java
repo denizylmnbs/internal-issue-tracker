f92a2487bd563d6c0c24f37b156c7e51ce1d2e10
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-26 21:05:45 +0300

    feat(user): create reset password dto

---
+++ src/main/java/com/ist/internal_issue_tracker/user/dto/ResetPasswordRequest.java
@@ -0,0 +1,11 @@
+package com.ist.internal_issue_tracker.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ResetPasswordRequest(
+        @NotBlank(message = "New password cannot be blank")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String newPassword
+) {
+}
