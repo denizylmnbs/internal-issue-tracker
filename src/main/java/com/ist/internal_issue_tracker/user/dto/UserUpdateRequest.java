a5fad13bc4637dc34e2789f3bd70d8ceca5d0b38
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-26 19:15:41 +0300

    feat(user): add UserUpdateRequest DTO

--- src/main/java/com/ist/internal_issue_tracker/user/dto/UserUpdateRequest.java
+++ src/main/java/com/ist/internal_issue_tracker/user/dto/UserUpdateRequest.java
@@ -1,4 +1,26 @@
 package com.ist.internal_issue_tracker.user.dto;
 
-public record UserUpdateRequest() {
+import com.ist.internal_issue_tracker.user.internal.EmailNormalizer;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserUpdateRequest(
+        @NotBlank(message = "Name cannot be blank")
+        @Size(min = 2, max = 255)
+        String name,
+
+        @NotBlank(message = "Surname cannot be blank")
+        @Size(min = 2, max = 255)
+        String surname,
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email is not valid")
+        String email
+
+
+) {
+    public UserUpdateRequest {
+        email = EmailNormalizer.normalize(email);
+    }
 }
