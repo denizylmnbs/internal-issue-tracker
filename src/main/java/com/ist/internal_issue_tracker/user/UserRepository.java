ab5c66a465b0276a32cb372471fbca127c27b8bb
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-27 16:05:43 +0300

    feat(user): add findByEmail query to UserRepository

--- src/main/java/com/ist/internal_issue_tracker/user/UserRepository.java
+++ src/main/java/com/ist/internal_issue_tracker/user/UserRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     Boolean existsByEmail(String email);
 
     Boolean existsByEmailAndIdNot(String email, Integer id);
+
+    Optional<User> findByEmail(String email);
 
     @Query("""
             SELECT u FROM User u
