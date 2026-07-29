82fbab8d9ff4941059955a591550d2c861349003
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-29 17:36:25 +0300

    exception(team): add team member error codes

---
+++ src/main/java/com/ist/internal_issue_tracker/team/exception/TeamMemberErrorCode.java
@@ -0,0 +1,34 @@
+package com.ist.internal_issue_tracker.team.exception;
+
+import com.ist.internal_issue_tracker.shared.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum TeamMemberErrorCode implements ErrorCode {
+  USER_NOT_FOUND(HttpStatus.UNPROCESSABLE_CONTENT, "This user not exist."),
+  USER_ROLE_NOT_ENOUGH(HttpStatus.FORBIDDEN, "User role is not enough to perform this action."),
+  TEAM_NOT_FOUND(HttpStatus.UNPROCESSABLE_CONTENT, "This team not exist."),
+  TEAM_MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "This user already exist in this team.");
+
+  private final HttpStatus httpStatus;
+  private final String message;
+
+  TeamMemberErrorCode(HttpStatus httpStatus, String message) {
+    this.httpStatus = httpStatus;
+    this.message = message;
+  }
+
+  @Override
+  public HttpStatus status() {
+    return httpStatus;
+  }
+
+  @Override
+  public String defaultMessage() {
+    return message;
+  }
+
+  @Override
+  public String code() {
+    return name();
+  }
+}
