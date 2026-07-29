e1e16a55a4f2aed218c4f257e3e84eb1dfad63fa
Deniz Yalmanbaş <denizylmnbsss@gmail.com>
2026-07-29 12:35:33 +0300

    exception(user): add role change not permitted exception to user error code

--- src/main/java/com/ist/internal_issue_tracker/user/exception/UserErrorCode.java
+++ src/main/java/com/ist/internal_issue_tracker/user/exception/UserErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
   EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "This email already exists."),
   CURRENT_PASSWORD_INCORRECT(HttpStatus.BAD_REQUEST, "Current password is incorrect."),
-  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Email or password is incorrect.");
+  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Email or password is incorrect."),
+  ROLE_CHANGE_NOT_PERMITTED(HttpStatus.FORBIDDEN, "Changing user role is not permitted.");
 
   private final HttpStatus httpStatus;
   private final String message;
